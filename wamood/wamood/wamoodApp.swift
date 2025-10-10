307d796222acac2fc20ea1d28fc7c5b67f1f118e
zyreoo <simone.marton89@gmail.com>
2025-10-10 11:58:00 +0300

    learn swift language and a few syntax and try to make some functions

---
+++ wamood/wamood/wamoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  wamoodApp.swift
+//  wamood
+//
+//  Created by Simone Marton on 09.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct wamoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
